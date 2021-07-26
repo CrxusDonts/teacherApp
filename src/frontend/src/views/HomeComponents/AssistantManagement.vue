2221714da40dfc7cdb38b4be1614a7a7eef1c0d3
高 祎康 <1913074@mail.nankai.edu.cn>
2021-07-26 22:38:41 +0800

    Create switchclass and other vues. Ref #10

---
+++ src/frontend/src/views/HomeComponents/AssistantManagement.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    name: 'AssistantManagement'
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/frontend/src/views/HomeComponents/HomeworkManagement.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    name: 'HomeworkManagement'
+};
+</script>
+
+<style scoped>
+
+</style>
