c9bb4c05dd26436b32fabc83c3644e33e3df2e6a
子果 程 <2746738323@qq.com>
2021-07-26 15:34:07 +0000

    完成登陆后后台管理的主页面

---
+++ src/frontend/src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-card class="common-header">
+    <a>
+      <img src="../assets/icon3.jpg" alt="" width="55px" style="float: left;
+                margin-top: -5px;">
+    </a>
+    <span style="position: absolute;
+    left: 100px;
+    font-weight: bold;
+    font-size: 32px;">布置作业系统</span>
+    <el-dropdown :show-timeout=10 :hide-timeout=50 style="float: right;
+    padding-right: 10px;">
+      <div class="el-dropdown-link">
+        <span>你好,{{userName}}</span>
+        <span style="float: right;
+                    margin-left: 10px;">
+          <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" :size=40></el-avatar>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item :divided=true @click.native="passwordModify">修改密码</el-dropdown-item>
+          <el-dropdown-item :divided=true @click.native="exit">退出登录</el-dropdown-item>
+        </el-dropdown-menu>
+        <i class="el-icon-arrow-down el-icon--right"></i>
+      </div>
+    </el-dropdown>
+  </el-card>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: ['userName'],
+    data: function () {
+        return {};
+    },
+    methods: {
+        passwordModify() {
+            this.$router.push({path: '/passwordModify', query: {userName: this.userName}});
+        },
+        exit() {
+            this.$router.push({path: '/'});
+        }
+    }
+};
+</script>
+
+<style scoped>
+.common-header {
+    height: 80px;
+    line-height: 40px;
+    opacity: .85;
+}
+
+.el-dropdown-link {
+    color: #409eff;
+    cursor: pointer;
+}
+
+.el-icon-arrow-down {
+    font-size: 12px;
+}
+</style>
---
+++ src/frontend/src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <Header v-bind:userName="userName"></Header>
+        <el-menu class="elMenu" default-active="homeworkManagement" mode="horizontal" @select="handleSelect"
+                 :router="true">
+            <el-menu-item index="homeworkManagement">作业管理</el-menu-item>
+            <el-submenu index="management">
+                <template slot="title">班级管理</template>
+                <el-menu-item index="studentManagement">学生管理</el-menu-item>
+                <el-menu-item index="assistantManagement">助教管理</el-menu-item>
+            </el-submenu>
+            <el-submenu index="grade">
+                <template slot="title">成绩统计</template>
+                <el-menu-item index="gradeByClass">按班级统计</el-menu-item>
+                <el-menu-item index="gradeByStudent">按学生统计</el-menu-item>
+            </el-submenu>
+            <el-menu-item index="switchClass">班级切换</el-menu-item>
+        </el-menu>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+
+export default {
+    name: 'Home',
+    data() {
+        return {
+            activeIndex: 'homeworkManagement',
+            userName: 'admin'
+        };
+    },
+    components: {Header},
+    mounted: function () {
+        this.userName = this.$route.query.userName;
+    },
+    methods: {
+        handleSelect(key) {
+            this.$router.push({path: key});
+            // console.log(key, keyPath);
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+.elMenu {
+    margin-top: 5px;
+    margin-right: 5px;
+    margin-left: 5px;
+}
+
+</style>
