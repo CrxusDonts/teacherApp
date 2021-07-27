9f9b6c482fdf7c1d549b6a919cf1519fb60e2542
子果 程 <2746738323@qq.com>
2021-07-27 08:09:38 +0000

    完成查看学生界面的设计

--- src/frontend/src/views/HomeComponents/StudentManagement.vue
+++ src/frontend/src/views/HomeComponents/StudentManagement.vue
@@ -1,12 +1,55 @@
 <template>
-  <div>
-
-  </div>
+    <el-table
+        :data="tableData"
+        stripe
+        style="width: 100%;
+            margin-top: 10px;">
+        <el-table-column align="center"
+                         type="index"
+                         :width="100">
+        </el-table-column>
+        <el-table-column
+            prop="id"
+            label="学号"
+            width="180">
+        </el-table-column>
+        <el-table-column
+            prop="name"
+            label="姓名"
+            width="180">
+        </el-table-column>
+        <el-table-column
+            prop="gender"
+            label="性别">
+        </el-table-column>
+        <el-table-column
+            align="right"
+            label="操作">
+            <template slot-scope="scope">
+                <el-button plain icon="el-icon-edit" size="mini">编辑学生</el-button>
+                <el-button type="danger" plain icon="el-icon-delete" size="mini">移除学生</el-button>
+            </template>
+        </el-table-column>
+    </el-table>
 </template>
 
 <script>
 export default {
-    name: 'StudentManagement'
+    name: 'StudentManagement',
+    data() {
+        return {
+            search: '',
+            tableData: [{
+                id:'12',
+                name: '果果果',
+                gender: '男',
+            }, {
+                id:'19',
+                name: '王小虎',
+                gender: '男',
+            }]
+        };
+    }
 };
 </script>
 
