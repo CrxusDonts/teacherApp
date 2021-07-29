07e32e00da30967145c5bffd30f561fc31b1a481
继煜 董 <djy1291923247@foxmail.com>
2021-07-29 15:43:30 +0000

    完成BackendAccount视图部分单元测试

--- src/backend/teacherApp/tests.py
+++ src/backend/teacherApp/tests.py
@@ -1,3 +1,36 @@
+import json
+from django.contrib.auth.models import User
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class BackendAccountViewTest(TestCase):
+    def setUp(self):
+        self.user_name = 'test'
+        self.password = 'password4test'
+        self.class_name = 'class4test'
+
+    def test_register_teacher(self):
+        test_data = json.dumps({
+            'user_name': self.user_name,
+            'password': self.password,
+            'class_name': self.class_name
+        })
+        response = self.client. \
+            post(path='http://localhost:8002/teacherApp/BackendAccount/register_teacher/',
+                 data=test_data, content_type='application/json')
+        print(response.data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_change_password(self):
+        User.objects.create_user(username=self.user_name, password=self.password)
+        self.client.login(username=self.user_name, password=self.password)
+        test_data = json.dumps({
+            'old_password': self.password,
+            'new_password': '123456'
+        })
+        response = self.client. \
+            put(path='http://localhost:8002/teacherApp/BackendAccount/change_password/',
+                data=test_data, content_type='application/json')
+        print(response.data)
+        self.assertEqual(response.status_code, 200)
